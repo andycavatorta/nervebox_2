73a2f2f800fb0943e932af00cbe7bb91070b9fdc
andycavatorta <andycavatorta@gmail.com>
2015-11-07 16:22:19 -0500

    =added nerveOSC.py

---
+++ common/nerveOSC.py
@@ -0,0 +1,42 @@
+"""
+nerveOSC messages
+
+format:
+token types:
+	path/value 
+	time - @time
+	env - {name:value, name:value}
+
+implementing only path and value for now
+
+"""
+import socket
+
+def decode(nerveOSC_str):
+	nerveOSC_l = nerveOSC_str.split(" ")
+	path = None
+	innerpath = ""
+	value = None
+	time = None
+	env = None
+	for token in nerveOSC_l:
+		if token[0] == "/":
+			path = token
+		if token[0] == "@":
+			time = token
+		if token[0] == "{":
+			env = token
+	path_l = path.split("/")
+	value  = path_l[-1:][0]
+	for i in range(len(path_l)-3):
+		innerpath += "/%s" % path_l[i+2]
+	return {
+		"path":path,
+		"innerpath":innerpath,
+		"value":value,
+		"time":time,
+		"env":env
+	}
+
+def encode(innerpath, value):
+	return "/%s%s/%s" % (socket.gethostname(),innerpath,str(value))
