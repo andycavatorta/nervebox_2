96d770f05e6cddc2705648e6f51624db1153350c
João Costa <joao.costa@nyu.edu>
2016-07-20 15:57:37 -0400

    Combined Server and Client into one file

--- client/main.py
+++ client/main.py
@@ -19,13 +19,16 @@ CLIENT_PATH = "%s/client/" % (BASE_PATH )
 DEVICES_PATH = "%s/client/devices/" % (BASE_PATH )
 COMMON_PATH = "%s/common/" % (BASE_PATH )
 HOST_SPECIFIC_PATH = "%s/client/devices/%s/" % (BASE_PATH, HOSTNAME)
-clientnames = ("Joaos-MacBook-Pro-2.local")
+SERVER_PATH = "%s/server/" % (BASE_PATH )
+STORE_PATH = "%s/store/" % SERVER_PATH
+clientnames = ()
 ROLE = "client" if HOSTNAME in clientnames else "server" 
 
 # local paths
 sys.path.append(BASE_PATH)
 sys.path.append(COMMON_PATH)
 sys.path.append(CLIENT_PATH)
+sys.path.append(SERVER_PATH)
 sys.path.append(HOST_SPECIFIC_PATH)
 
 # import local modules
@@ -34,15 +37,30 @@ import nerveOSC
 if ROLE == "client":
     import device
     device.init()
+else:
+    import parseOsc
+    import midiToOsc
+    import midiDeviceManager
+    import midiserver
 
 # load config
 with open(COMMON_PATH + 'settings.json', 'r') as f:
     SETTINGS = json.load(f)
 
+with open(COMMON_PATH + 'mappings.json', 'r') as f:
+    MAPPINGS = json.load(f)
+
+MAPPING = MAPPINGS["MAPPINGS"]["default"] # todo: the mapping name will have to be dynamically updated
+
 ######################
 ##### NETWORKING #####
 ######################
 
-subscribernames = ["nervebox"]
+if ROLE == "client":
+    subscribernames = ["nervebox"]
+else:
+    subscribernames = filter(lambda x: os.path.isdir(os.path.join(DEVICES_PATH, x)), os.listdir(DEVICES_PATH))
+    subscribernames.append("nervebox2")
+    subscribernames.append("admins-MacBook-Pro.local")
 
 dps.init_networking(subscribernames,HOSTNAME,SETTINGS,ROLE)
