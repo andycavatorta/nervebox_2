dc1a41daea5ab3646f7a050e32267ee7a40bf806
João Costa <joao.costa@nyu.edu>
2016-07-28 15:52:17 -0400

    new main.py
    - implemented dashboard
    - logerror v1.0

--- client/main.py
+++ client/main.py
@@ -9,6 +9,8 @@ import time
 import threading
 import sys
 import socket
+import errorlog as elog
+
 
 # constants
 #PI_NATIVE = os.uname()[4].startswith("arm") # TRUE if running on RPi
@@ -21,8 +23,12 @@ COMMON_PATH = "%s/common/" % (BASE_PATH )
 HOST_SPECIFIC_PATH = "%s/client/devices/%s/" % (BASE_PATH, HOSTNAME)
 SERVER_PATH = "%s/server/" % (BASE_PATH )
 STORE_PATH = "%s/store/" % SERVER_PATH
-clientnames = ()
-ROLE = "client" if HOSTNAME in clientnames else "server" 
+clientnames = ("blueberrypie")
+ROLE = "client" if HOSTNAME in clientnames else "server"
+response = raw_input("Dashboard (yes/no)?")
+if response == 'yes':
+    ROLE = 'dashboard'
+print ROLE
 
 # local paths
 sys.path.append(BASE_PATH)
@@ -31,36 +37,50 @@ sys.path.append(CLIENT_PATH)
 sys.path.append(SERVER_PATH)
 sys.path.append(HOST_SPECIFIC_PATH)
 
-# import local modules
-import dps
-import nerveOSC
-if ROLE == "client":
-    import device
-    device.init()
-else:
-    import parseOsc
-    import midiToOsc
-    import midiDeviceManager
-    import midiserver
+try:
+    # import local modules
+    import dps
+    import nerveOSC
+    if ROLE == "client":
+        import device
+        device.init()
+    else:
+        import parseOsc
+        import midiToOsc
+        import midiDeviceManager
+        import midiserver
 
-# load config
-with open(COMMON_PATH + 'settings.json', 'r') as f:
-    SETTINGS = json.load(f)
+    # load config
+    with open(COMMON_PATH + 'settings.json', 'r') as f:
+        SETTINGS = json.load(f)
 
-with open(COMMON_PATH + 'mappings.json', 'r') as f:
-    MAPPINGS = json.load(f)
+    with open(COMMON_PATH + 'mappings.json', 'r') as f:
+        MAPPINGS = json.load(f)
 
-MAPPING = MAPPINGS["MAPPINGS"]["default"] # todo: the mapping name will have to be dynamically updated
+    MAPPING = MAPPINGS["MAPPINGS"]["default"] # todo: the mapping name will have to be dynamically updated
 
-######################
-##### NETWORKING #####
-######################
+    ######################
+    ##### NETWORKING #####
+    ######################
 
-if ROLE == "client":
-    subscribernames = ["nervebox"]
-else:
-    subscribernames = filter(lambda x: os.path.isdir(os.path.join(DEVICES_PATH, x)), os.listdir(DEVICES_PATH))
-    subscribernames.append("nervebox2")
-    subscribernames.append("admins-MacBook-Pro.local")
+    if ROLE == "client":
+        subscribernames = ["nervebox"]
+    else:
+        subscribernames = filter(lambda x: os.path.isdir(os.path.join(DEVICES_PATH, x)), os.listdir(DEVICES_PATH))
+        subscribernames.append("nervebox2")
+        subscribernames.append(HOSTNAME)
 
-dps.init_networking(subscribernames,HOSTNAME,SETTINGS,ROLE)
+    dps.init_networking(
+        subscribernames,
+        HOSTNAME,
+        ROLE,
+        SETTINGS["pubsub_pubPort"],
+        SETTINGS["pubsub_pubPort2"],
+        SETTINGS["discovery_multicastGroup"],
+        SETTINGS["discovery_multicastPort"],
+        SETTINGS["discovery_multicastPort2"],
+        SETTINGS["discovery_responsePort"],
+        SETTINGS["discovery_responsePort2"]
+    )
+except:
+    elog.elog.logerror()
